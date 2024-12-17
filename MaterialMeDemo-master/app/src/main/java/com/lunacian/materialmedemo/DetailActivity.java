83070a1a632f0173abba2018bcbeb8df5ea7fc02
ncl29 <mikyllanicolen@gmail.com>
2024-12-17 17:31:20 +0800

    Add files via upload

---
+++ MaterialMeDemo-master/app/src/main/java/com/lunacian/materialmedemo/DetailActivity.java
@@ -0,0 +1,36 @@
+package com.lunacian.materialmedemo;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_detail);
+
+        TextView sportsTitle = findViewById(R.id.titleDetail);
+        ImageView sportsImage = findViewById(R.id.sportsImageDetail);
+
+        sportsTitle.setText(getIntent().getStringExtra("title"));
+
+        Glide.with(this).load(getIntent().getIntExtra("image_resource", 0)).into(sportsImage);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
